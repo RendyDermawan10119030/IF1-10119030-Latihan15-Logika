3164e0a23a2e087c1039df4b290bbfff08623d4b
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:57:51 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan15/logika/IF110119030Latihan15Logika.java
@@ -0,0 +1,18 @@
+package if1.pkg10119030.latihan15.logika;
+/*
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Relational
+ */    
+public class IF110119030Latihan15Logika {
+
+    public static void main(String[] args) {
+    boolean a = true;
+    boolean b = false;
+    System.out.println("a && b = " + (a && b));
+    System.out.println("a || b = " + (a || b));
+    System.out.println("!a = " + !a);
+    System.out.println("!(a && b) = " + !(a && b));
+    }  
+}
